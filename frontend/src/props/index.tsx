9f67aff61e717383a147436e23feae651cb2a5f0
Hamid Rezaeiard <hamid.rezaeirad@gmail.com>
2025-01-29 00:02:43 +0100

    chore [#15] remove unnecessary onSetAlert props, It replace with context

--- frontend/src/props/index.tsx
+++ frontend/src/props/index.tsx
@@ -1,4 +1,2 @@
-export type { AddSynonymsProps } from "./AddSynonymsProps";
 export type { SearchBarProps } from "./SearchBarProps";
-export type { SearchSynonymsProps } from "./SearchSynonymsProps";
 export type { TagInputProps } from "./TagInputProps";
