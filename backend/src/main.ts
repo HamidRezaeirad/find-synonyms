52cf1b76e84494f41ec10f58cb8deb8c404df31b
Hamid Rezaeiard <hamid.rezaeirad@gmail.com>
2025-01-28 09:02:22 +0100

    feat [#1] init backend project

---
+++ backend/src/main.ts
@@ -0,0 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Synonyms Tool')
+    .setDescription('Synonyms Tool API')
+    .setVersion('1.0')
+    .addTag('Synonyms Tool')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
